286d140d4a720c2ed4d67343b374041ea922803f
NickWang <brotherstyx@gmail.com>
2022-08-08 14:40:53 +0800

    fix: 🐛 var naming

--- src/client/puppet-service.ts
+++ src/client/puppet-service.ts
@@ -445,7 +445,7 @@ class PuppetService extends PUPPET.Puppet {
       [PUPPET.types.Dirty.Post]:         async (_: string) => {},
       [PUPPET.types.Dirty.Room]:         async (id: string) => this._payloadStore.room?.delete(id),
       [PUPPET.types.Dirty.RoomMember]:   async (id: string) => this._payloadStore.roomMember?.delete(id),
-      [PUPPET.types.Dirty.Tag]:          async (key: string) => this._payloadStore.tag?.delete(key),
+      [PUPPET.types.Dirty.Tag]:          async (id: string) => this._payloadStore.tag?.delete(id),
       [PUPPET.types.Dirty.TagGroup]:     async (id: string) => this._payloadStore.tagGroup?.delete(id),
       [PUPPET.types.Dirty.Unspecified]:  async (id: string) => { throw new Error('Unspecified type with id: ' + id) },
     }
