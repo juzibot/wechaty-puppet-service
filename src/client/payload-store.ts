735dae57822d353980a71e29a5ae0f873e3ea974
NickWang <brotherstyx@gmail.com>
2023-05-23 19:26:23 +0800

    fix: 🐛 close miscellaneous when destroy

--- src/client/payload-store.ts
+++ src/client/payload-store.ts
@@ -155,6 +155,8 @@ class PayloadStore {
       throw new Error('Can not destroy() a start()-ed store. Call stop() to stop it first')
     }
 
+    await this.miscellaneous.close()
+
     /**
      * Huan(202108): `fs.rm` was introduced from Node.js v14.14
      *  https://nodejs.org/api/fs.html#fs_fspromises_rm_path_options
